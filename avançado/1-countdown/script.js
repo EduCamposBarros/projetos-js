e1cc0d4b57a878c1fcfe8e2aab2ad83724173bcd
Eduardo <eduardocampos97@hotmail.com>
2021-01-11 14:54:22 -0400

    desafio Js - Avançado

---
+++ avançado/1-countdown/script.js
@@ -0,0 +1,32 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secsEl = document.getElementById('secs');
+
+const anoNovo =  '1 Jan 2022';
+
+function countdown(){
+    const dataAnoNovo = new Date(anoNovo);
+    const dataAtual = new Date();
+
+    const segundosTotal = (dataAnoNovo - dataAtual) / 1000;
+
+    const dias = Math.floor(segundosTotal / 3600 / 24);
+
+    const horas = Math.floor(segundosTotal / 3600) % 24;
+
+    const minutos = Math.floor(segundosTotal / 60)% 60;
+
+    const segundos = Math.floor(segundosTotal) % 60;
+
+    daysEl.innerHTML = dias;
+    hoursEl.innerHTML = horas;
+    minsEl.innerHTML = minutos;
+    secsEl.innerHTML = segundos;
+
+};
+
+//Chamado da função
+setInterval(countdown, 1000);
+
+countdown();
